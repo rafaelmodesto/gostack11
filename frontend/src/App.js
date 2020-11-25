4ad61d1a11e038899495534e77117eabbe674b4f
Rafael Modesto <rafaelmodesto09@gmail.com>
2020-11-24 23:27:09 -0300

    Conceitos ReactJS, aplicando conceitos de estado, imutabilidade e importando css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './App.css';
 
 import Header from './components/Header';
 
 function App() {
+  //aplicando conceito de estado(useState)
+  const [projects, setProjects ] = useState(['Desenvolvimento de app', 'Front-end web']);
+
+  function handleAddProject() {
+    //projects.push(`Novo projeto ${Date.now()}`);
+
+    /*
+    * Anotação 01
+    * '...'(spread operation) utilizado pra copiar todo valor que já existe no 
+    * array, e adicionar o que está após ao operador.
+    * 
+    * Anotação 02
+    * aplicando imutabilidade, gerando um novo array com as informações originais + novas informações.
+    */
+    setProjects([...projects, `Novo projeto ${Date.now()}`])  
+  }
+
   //Compenente recebendo propriedade "title" 
   return (
-    <div>      
-      <Header title="Homepage">
-        <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
+    <>      
+      <Header title="Projects" />
+
       <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-        </ul>
-      </Header>
-    </div>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+    </>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,11 +3,10 @@ import React, { Children } from 'react';
 /*Utilizando prop "children" que é uma prop que já vem por padrão nos componentes,
 * usamos para trazer o conteúdo do nosso componente.
 */
-export default function Header({title, children}/*ou props*/) {
+export default function Header({title}/*ou props*/) {
   return (
     <header>
       <h1>{title}</h1>
-      {children}
     </header>
   );
 }
