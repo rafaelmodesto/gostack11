0719619e450333cad2586703cd54c65cd03b57e9
Rafael Modesto <rafaelmodesto09@gmail.com>
2020-12-01 23:08:08 -0300

    Evoluindo front-end, integrando com api trazendo e inserindo novos dados.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
 
@@ -6,9 +7,15 @@ import Header from './components/Header';
 
 function App() {
   //aplicando conceito de estado(useState)
-  const [projects, setProjects ] = useState(['Desenvolvimento de app', 'Front-end web']);
+  const [projects, setProjects ] = useState([]);
 
-  function handleAddProject() {
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
     //projects.push(`Novo projeto ${Date.now()}`);
 
     /*
@@ -19,7 +26,16 @@ function App() {
     * Anotação 02
     * aplicando imutabilidade, gerando um novo array com as informações originais + novas informações.
     */
-    setProjects([...projects, `Novo projeto ${Date.now()}`])  
+   // setProjects([...projects, `Novo projeto ${Date.now()}`])  
+
+    const response = await api.post('projects', {
+     title: `Novo projeto ${Date.now()}`,
+     owner: "Rafael Modesto"
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   //Compenente recebendo propriedade "title" 
@@ -28,7 +44,7 @@ function App() {
       <Header title="Projects" />
 
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
