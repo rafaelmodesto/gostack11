e84953e1007c8279e760a9c765c528a606218641
Rafael Modesto <rafaelmodesto09@gmail.com>
2020-12-27 18:27:15 -0300

    Incluindo itens atraves do app

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 
 import api from './services/api';
 
@@ -12,6 +12,16 @@ export default function App() {
       setProjects(response.data);
     });
   }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: 'Rafael Modesto'
+    });
+
+    const project = response.data;
+    setProjects([...projects, project]);
+  }
 
   return(
     //Utilizando fragment pois não posso ter dois components juntos sem
@@ -27,6 +37,10 @@ export default function App() {
           <Text style={styles.project}>{project.title}</Text>
         )}
       />
+
+      <TouchableOpacity activeOpacity={0.6} style={styles.button} onPress={handleAddProject}>
+        <Text style={styles.buttonText}>Adicionar Projeto</Text>
+      </TouchableOpacity>
     </SafeAreaView>
 
     </>
@@ -44,5 +58,19 @@ const styles = StyleSheet.create({
   project: {
     color: '#FFF',
     fontSize: 20,
+  },
+
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
